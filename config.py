c45d0ac208a5bc0ffb18e54a49a279a2a8c14013
FatalX2080 <maks.belozerov.06@mail.ru>
2025-09-14 16:52:32 +0300

    took out some params to config.py

---
+++ config.py
@@ -0,0 +1,35 @@
+from flet import Colors
+
+max_journal_num = 34
+
+flet_colors_list = [
+    Colors.WHITE,
+    Colors.BLUE,
+    Colors.CYAN,
+    Colors.GREEN,
+    Colors.YELLOW,
+    Colors.PINK,
+    Colors.ORANGE,
+    Colors.RED,
+    Colors.PURPLE,
+    Colors.BROWN,
+]
+
+colors_list = [
+    "#0000ff",
+    "#00ffff",
+    "#00ff00",
+    "##ffde21",
+    "#ff8da1",
+    "#ffa500",
+    "#ff0000",
+    "#895129",
+    "#ffffff"
+]
+
+graph_attr = {'rankdir': 'LR', 'bgcolor': 'white', 'dpi': '300'}
+node_attr = {'shape': 'circle', 'style': 'filled', 'fillcolor': 'lightblue', 'width': '0.6',
+             'height': '0.6'}
+edge_attr = {'arrowsize': '0.8'}
+
+rejection_message = "Haha, do you want another core? Well, do it. We're tired of writing the python core, so the rest is up to you."
